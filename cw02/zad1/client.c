35492011a365288f80649469f35c4c15ebcd171d
Devedir <109344310+Devedir@users.noreply.github.com>
2024-03-16 11:18:36 +0100

    Lab2 finished

--- cw02/zad1/client.c
+++ cw02/zad1/client.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
-#include "collatz.h"
+#ifndef DYNAMIC
+    #include "collatz.h"
+#else
+    #include <dlfcn.h>
+#endif
 
 #define MAX_ITER 10000
 
 int main(void) {
+#ifdef DYNAMIC
+    void* handle = dlopen("./libcollatz.so", RTLD_LAZY);
+    char* error = dlerror();
+    if (!handle) {
+        fprintf(stderr, "%s\n", error);
+        return 1;
+    }
+    int (*test_collatz_convergence)(int, int) = dlsym(handle, "test_collatz_convergence");
+    error = dlerror();
+    if (error != NULL) {
+        fprintf(stderr, "%s\n", error);
+        dlclose(handle);
+        return 2;
+    }
+#endif
+
     printf("Dla 2137: %d\n", test_collatz_convergence(2137, MAX_ITER));
     printf("Dla 321: %d\n", test_collatz_convergence(321, MAX_ITER));
+
+#ifdef DYNAMIC
+    dlclose(handle);
+#endif
+
     return 0;
 }
