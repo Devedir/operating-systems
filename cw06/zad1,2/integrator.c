db655dc4ebe02ea4735477ff9924150229ab8d80
Devedir <109344310+Devedir@users.noreply.github.com>
2024-04-21 01:18:02 +0200

    Calculating areas and sending them back

--- cw06/zad1,2/integrator.c
+++ cw06/zad1,2/integrator.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+
+double fun(double x) {
+    return 4 / (x*x + 1);
+}
 
 int main(void) {
     // Wait and read the calculation parameters from a named pipe
@@ -17,11 +26,37 @@ int main(void) {
         return 5;
     }
 
-    printf("Received: %lf %d %lf %lf", h, n, boundaries[0], boundaries[1]);
-
-    // Create n subprocesses to calculate a specific area
+    // Create n child processes to calculate a specific area
+    int* read_ends = malloc(sizeof(int) * n);
+    double x = boundaries[0];
+    double dist = (boundaries[1] - boundaries[0]) / n;
+    for (int i = 0; i < n; i++) {
+        int pipedes[2];
+        if (pipe(pipedes) == -1) {
+            perror("Error creating a pipe");
+            free(read_ends);
+            return 10;
+        }
+        pid_t pid = fork();
+        if (pid == -1) {
+            fprintf(stderr, "Error on forking nr %d: %s", i, strerror(errno));
+            free(read_ends);
+            return 11;
+        }
+        else if (pid == 0) {
+            close(pipedes[0]);
+            free(read_ends);
+            double area = h * fun(x);
+            if (write(pipedes[1], &area, sizeof area) == -1) return 1;
+            return 0;
+        }
+        close(pipedes[1]);
+        read_ends[i] = pipedes[0];
+        x += dist;
+    }
 
     // Wait and read their results summing them up
+    free(read_ends);
 
     // Send back the result through a named pipe
 
