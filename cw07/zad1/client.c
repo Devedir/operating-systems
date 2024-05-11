800261b73ae24b8d2425f9e1d5a5953a3c074c56
Devedir <109344310+Devedir@users.noreply.github.com>
2024-05-11 12:34:13 +0200

    Message queue INIT

--- cw07/zad1/client.c
+++ cw07/zad1/client.c
@@ -1,3 +1,48 @@
-int main(int argc, char* argv[]) {
+#include <mqueue.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void) {
+    char name[18];
+    snprintf(name, 18, "/chat-client-%d", getpid());
+    mqd_t my_queue = mq_open(name, O_RDONLY | O_CREAT, 077777, NULL);
+    if (my_queue == (mqd_t) -1) {
+        perror("Client queue creating error");
+        return 1;
+    }
+
+    mqd_t server_queue = mq_open("/chat-server", O_WRONLY);
+    if (server_queue == (mqd_t) -1) {
+        perror("Server queue opening error");
+        return 2;
+    }
+    char message[22];
+    strcat(message, "INIT");
+    strcat(message, name);
+    int sending = mq_send(server_queue, message, 22, 2);
+    if (sending == -1) {
+        perror("Server queue sending error");
+        return 3;
+    }
+    int closing = mq_close(server_queue);
+    if (closing == -1) {
+        perror("Server queue closing error");
+        return 4;
+    }
+
+    closing = mq_close(my_queue);
+    if (closing == -1) {
+        perror("Client queue closing error");
+        return 5;
+    }
+    int unlinking = mq_unlink(name);
+    if (unlinking == -1) {
+        perror("Client queue unlinking error");
+        return 6;
+    }
+
     return 0;
 }
--- cw07/zad1/server.c
+++ cw07/zad1/server.c
@@ -1,3 +1,36 @@
-int main(int argc, char* argv[]) {
+#include <mqueue.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#define MESSAGE_SIZE 8192
+
+int main(void) {
+    const char name[] = "/chat-server";
+    mqd_t server_queue = mq_open(name, O_RDONLY | O_CREAT, 0777, NULL);
+    if (server_queue == (mqd_t) -1) {
+        perror("Queue creating error");
+        return 1;
+    }
+
+    char message[MESSAGE_SIZE];
+    ssize_t recieving = mq_receive(server_queue, message, MESSAGE_SIZE, NULL);
+    if (recieving == (ssize_t) -1) {
+        perror("Queue receiving error");
+        return 2;
+    }
+    printf("%d:%s\n", server_queue, message);
+
+    int closing = mq_close(server_queue);
+    if (closing == -1) {
+        perror("Queue closing error");
+        return 3;
+    }
+    int unlinking = mq_unlink(name);
+    if (unlinking == -1) {
+        perror("Queue unliking error");
+        return 4;
+    }
+
     return 0;
 }
