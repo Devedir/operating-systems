8b208c24f534f67d3c5cd04280e8fa480245ce0b
Devedir <109344310+Devedir@users.noreply.github.com>
2024-05-11 16:27:39 +0200

    Registering system

--- cw07/zad1/client.c
+++ cw07/zad1/client.c
@@ -1,48 +1,76 @@
 #include <mqueue.h>
 #include <fcntl.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
+#define QUEUE_NAME_LEN 18
+#define MESSAGE_SIZE 8192
+
 int main(void) {
-    char name[18];
-    snprintf(name, 18, "/chat-client-%d", getpid());
+    char name[QUEUE_NAME_LEN];
+    char pidbuff[11] = {'\0'};
+    int pidlen = snprintf(pidbuff, 10, "%d", getpid());
+    char pidstr[5];
+    strncpy(pidstr, pidbuff + pidlen - 4, 5);
+    snprintf(name, QUEUE_NAME_LEN, "/chat-client-%s", pidstr);
+
     mqd_t my_queue = mq_open(name, O_RDONLY | O_CREAT, 077777, NULL);
     if (my_queue == (mqd_t) -1) {
         perror("Client queue creating error");
         return 1;
     }
-
     mqd_t server_queue = mq_open("/chat-server", O_WRONLY);
     if (server_queue == (mqd_t) -1) {
         perror("Server queue opening error");
         return 2;
     }
-    char message[22];
+
+    char message[MESSAGE_SIZE];
     strcat(message, "INIT");
     strcat(message, name);
-    int sending = mq_send(server_queue, message, 22, 2);
+    int sending = mq_send(server_queue, message, QUEUE_NAME_LEN + 4, 2);
     if (sending == -1) {
         perror("Server queue sending error");
         return 3;
     }
-    int closing = mq_close(server_queue);
-    if (closing == -1) {
-        perror("Server queue closing error");
+
+    mqd_t id;
+    ssize_t receiving = mq_receive(my_queue, (char*) &id, MESSAGE_SIZE, NULL);
+    if (receiving == (ssize_t) -1) {
+        perror("Client queue receiving error");
         return 4;
     }
 
+    while (id != (mqd_t) -1) {
+        receiving = mq_receive(my_queue, message, MESSAGE_SIZE, NULL);
+        if (receiving == (ssize_t) -1) {
+            perror("Client queue receiving error");
+            continue;
+        }
+        if (strncmp("CLOSE", message, 5) == 0) {
+            break;
+        }
+        printf("%s\n", message);
+    }
+
+    int returning = 0;
+
+    int closing = mq_close(server_queue);
+    if (closing == -1) {
+        perror("Server queue closing error");
+        returning = 5;
+    }
     closing = mq_close(my_queue);
     if (closing == -1) {
         perror("Client queue closing error");
-        return 5;
+        returning = 6;
     }
     int unlinking = mq_unlink(name);
     if (unlinking == -1) {
         perror("Client queue unlinking error");
-        return 6;
+        returning = 7;
     }
 
-    return 0;
+    return returning;
 }
--- cw07/zad1/server.c
+++ cw07/zad1/server.c
@@ -2,8 +2,11 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MESSAGE_SIZE 8192
+#define MAX_NO_CLIENTS 5
+#define QUEUE_NAME_LEN 18
 
 int main(void) {
     const char name[] = "/chat-server";
@@ -13,15 +16,63 @@ int main(void) {
         return 1;
     }
 
+    mqd_t client_ids[MAX_NO_CLIENTS];
+    size_t registered_clients = 0;
     char message[MESSAGE_SIZE];
-    ssize_t recieving = mq_receive(server_queue, message, MESSAGE_SIZE, NULL);
-    if (recieving == (ssize_t) -1) {
-        perror("Queue receiving error");
-        return 2;
-    }
-    printf("%d:%s\n", server_queue, message);
+    int sending, closing;
 
-    int closing = mq_close(server_queue);
+    while (1) {
+        ssize_t receiving = mq_receive(server_queue, message, MESSAGE_SIZE, NULL);
+        if (receiving == (ssize_t) -1) {
+            perror("Queue receiving error");
+            return 2;
+        }
+        if (strncmp("CLOSE", message, 5) == 0) {
+            break;
+        }
+        if (strncmp("INIT", message, 4) == 0) {
+            char client_queue_name[QUEUE_NAME_LEN];
+            strncpy(client_queue_name, message + 4, QUEUE_NAME_LEN);
+            mqd_t new_client_id = mq_open(client_queue_name, O_WRONLY);
+            if (new_client_id == (mqd_t) -1) {
+                perror("Client queue opening on server error");
+                continue;
+            }
+
+            if (registered_clients == MAX_NO_CLIENTS) {
+                fprintf(stderr, "Cannot register: Reached the maximum number of clients.\n");
+                mqd_t closing_code = -1;
+                sending = mq_send(new_client_id, (char*) &closing_code,
+                                  sizeof(mqd_t), 3);
+                if (sending == -1)
+                    perror("Client queue sending error");
+                closing = mq_close(new_client_id);
+                if (closing == -1)
+                    perror("Client queue closing error");
+                break;  //TODO: delete this temporary closing gateway
+            } else {
+                printf("New client: %s\n", client_queue_name);
+                client_ids[registered_clients] = new_client_id;
+                registered_clients++;
+
+                sending = mq_send(new_client_id, (char*) &new_client_id,
+                                  sizeof(mqd_t), 2);
+                if (sending == -1)
+                    perror("Client queue sending error");
+            }
+        }
+    }
+
+    for (size_t i = 0; i < registered_clients; i++) {
+        sending = mq_send(client_ids[i], "CLOSE", 6, 3);
+        if (sending == -1)
+            perror("Client queue sending error");
+        closing = mq_close(client_ids[i]);
+        if (closing == -1)
+            perror("Client queue closing on server error");
+    }
+
+    closing = mq_close(server_queue);
     if (closing == -1) {
         perror("Queue closing error");
         return 3;
